330e51dd933a9723848fa457e7d4bd24c1e22fdd
Baha96 <87002133101a@gmail.com>
2019-06-14 12:33:24 +0600

    Добавил плагины для opencart

---
+++ opencart/новости/newsblog.20171002.opencart.20x-21x.ocmod/upload/catalog/language/ua-uk/newsblog/article.php
@@ -0,0 +1,19 @@
+<?php
+// Text
+$_['text_related']          = 'Схожі статті';
+$_['text_related_products'] = 'Рекомендуємі товари';
+$_['text_attributes']   	= 'Додаткові поля';
+$_['text_tags']             = 'Теги:';
+
+// Entry
+$_['entry_qty']             = 'Кіл-ть';
+$_['entry_name']            = 'Ваше ім&apos;я:';
+$_['entry_review']          = 'Ваш відгук';
+$_['entry_rating']          = 'Рейтинг';
+$_['entry_good']            = 'Добре';
+$_['entry_bad']             = 'Погано';
+
+// Error
+$_['error_name']            = 'Ім&apos;я має бути від 3 до 25 символів!';
+$_['error_text']            = 'Текст Відгука має бути від 25 до 1000 символів!';
+$_['error_rating']          = 'Будь ласка поставте оцінку!';
