e7034b2870db31100228083a9a565b875c861050
Baha96 <87002133101a@gmail.com>
2019-06-14 12:36:19 +0600

    для меня

---
+++ для меня/seo-meta.php
@@ -0,0 +1,139 @@
+<?php
+if ($_SERVER['REQUEST_URI'] == '/en/agrobiznes-2020/') {
+$seo_title = 'Agrobusiness 2020';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Special equipment on lease. Road-building machinery on lease';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/faq/') {
+$seo_title = 'Freaquently asked questions';
+$seo_descr = 'Freaquently asked questions about rendering of leasing services in Kazakhstan and “TechnoLeasing” main services';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-kompanii/news/60-/') {
+$seo_title = 'Presentation of the newest freight trucks KAMAZ-5490';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Special equipment on lease. Road-building machinery on lease';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-kompanii/news/63-/') {
+$seo_title = 'Run into Spring with “TechnoLeasing”';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Special equipment on lease. Road-building machinery on lease';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/sitemap/') {
+$seo_title = 'Sitemap';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Special equipment on lease. Road-building machinery on lease';
+     }      
+else if ($_SERVER['REQUEST_URI'] == '/en/stati/') {
+$seo_title = 'Articles';
+$seo_descr = 'The most interesting and useful articles tied to “TechnoLeasing” main services';
+     } 
+ 
+
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/traktora/') {
+$seo_title = 'Trucks on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Trucks on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/kombajn/') {
+$seo_title = 'Combine harvester threshers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Combine harvester threshers on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/seyalka/') {
+$seo_title = 'Seed planters on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Seed planters on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/kultivatory/') {
+$seo_title = 'Cultivators on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Cultivators on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/opryskivateli/') {
+$seo_title = 'Spraying machines on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Spraying machines on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/zhatka/') {
+$seo_title = 'Windrowers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Windrowers on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/kosilki/') {
+$seo_title = 'Mowing machines on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Mowing machines on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/press-podborshchik/') {
+$seo_title = 'Baling machines on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Baling machines on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/bunker-nakopitel/') {
+$seo_title = 'Storage hoppers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Baling machines on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/lizing-selskokhozyajstvennoj-tekhniki/valkoukladchik/') {
+$seo_title = 'Swathmakers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Agricultural equipment on lease. Swathmakers on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/avtogrejder/') {
+$seo_title = 'Land levelers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Land levelers on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/katki/') {
+$seo_title = 'Road rollers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Road rollers on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/asfaltoukladchik/') {
+$seo_title = 'Asphalt placers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Asphalt placers on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/betonoukladchik/') {
+$seo_title = 'Concrete placers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Concrete placers on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/gruzovoj-transport/') {
+$seo_title = 'Freight vehicle on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Freight vehicle on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/samosvaly/') {
+$seo_title = 'Tip trucks on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Tip trucks on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/tyagachi/') {
+$seo_title = 'Road tractor on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Road tractor on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/pritsepy/') {
+$seo_title = 'Truck trailers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Truck trailers on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/toplivozapravshchik/') {
+$seo_title = 'Fuel-servicing trucks on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Fuel-servicing trucks on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/dorozhno-stroitelnaya-tekhnika/tsisterny/') {
+$seo_title = 'Tanker trucks on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Road-building machinery on lease. Tanker trucks on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/spetstekhnika/ekskavatory/') {
+$seo_title = 'Shovelling machines on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Special equipment on lease. Shovelling machines on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/spetstekhnika/pogruzchik/') {
+$seo_title = 'Lift tracks on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Special equipment on lease. Lift tracks on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/spetstekhnika/krany/') {
+$seo_title = 'Crane trucks on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Special equipment on lease. Crane trucks on lease  by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/spetstekhnika/avtobetonosmesitel/') {
+$seo_title = 'Concrete mixers on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Special equipment on lease. Concrete mixers on lease by “TechnoLeasing” company';
+     } 
+else if ($_SERVER['REQUEST_URI'] == '/en/o-lizinge/spetstekhnika/buldozer/') {
+$seo_title = 'Soil shifters on lease';
+$seo_descr = 'Rendering of leasing services in Kazakhstan. Special equipment on lease. Soil shifters on lease by “TechnoLeasing” company';
+     } 
+
+
+
+
+
+
+
+
+
+
+
+?>
